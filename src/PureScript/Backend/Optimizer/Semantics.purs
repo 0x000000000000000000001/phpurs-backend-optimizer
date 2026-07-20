cc786c8f8ffdc14836c578c12e578bc96fbe726f
0x1 <kevin.francart3@gmail.com>
2026-07-20 21:52:57 +0200

    fix(Semantics): only clear locals for EvalExtern to fix scope bug

--- src/PureScript/Backend/Optimizer/Semantics.purs
+++ src/PureScript/Backend/Optimizer/Semantics.purs
@@ -942,10 +942,9 @@ snocSpine spine = case _ of
 envForGroup :: Env -> EvalRef -> InlineAccessor -> Array (Qualified Ident) -> Env
 envForGroup (Env e) ref acc group =
   let
-    moduleName = case ref of
-      EvalExtern (Qualified (Just mn) _) -> mn
-      _ -> e.currentModule
-    newEnv = Env e { locals = [], currentModule = moduleName }
+    newEnv = case ref of
+      EvalExtern (Qualified (Just mn) _) -> Env e { locals = [], currentModule = mn }
+      _ -> Env e
   in
     if Array.null group then newEnv
     else addStop newEnv ref acc
